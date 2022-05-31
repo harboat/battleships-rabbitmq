b55a54eed9dced44defbeb126396e5dd7789f1da
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-05-31 13:58:53 +0200

    Init commit

---
+++ src/main/java/com/github/harboat/rabbitmq/RabbitMQMessageProducer.java
@@ -0,0 +1,16 @@
+package com.github.harboat.rabbitmq;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class RabbitMQMessageProducer {
+
+    private final AmqpTemplate amqpTemplate;
+
+    public void publish(Object payload, String exchange, String routingKey) {
+        amqpTemplate.convertAndSend(exchange, routingKey, payload);
+    }
+}
